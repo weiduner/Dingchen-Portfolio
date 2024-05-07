d9f5c6b3a74796092bdaa59b817b686fd41a5a40
weiduner <84208389+weiduner@users.noreply.github.com>
2024-05-07 03:41:14 -0700

    added responsive design in project section

--- src/Component/project/projectCard/ProjectCard.jsx
+++ src/Component/project/projectCard/ProjectCard.jsx
@@ -7,15 +7,13 @@ const ProjectCard = ( {details} ) => {
         <div className="projectCard">
             <div className="title">
                 <p>{details.title}</p>
+            </div>
+            <div className="content">
                 <div className="skills">
                     {details.skills.map((skill)=>(
                         <div className="skill" key={skill}>{skill}</div>
                     ))}
                 </div>
-                
-            </div>
-            <div className="content">
-
                 <div className="description">{details.description}</div>
                 <div className="buttons">
                     <a href={details.demoSrc} target="_blank">View Demo</a>
